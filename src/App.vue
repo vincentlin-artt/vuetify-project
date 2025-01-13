4e573cafbe93aee46d59c934501dba2a357651a2
vincentlin-artt <vincent.lin1970@gmail.com>
2025-01-13 17:08:17 +0800

    add system-bar trial

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,50 @@
 <template>
+  <div>
+    <v-layout style="height: 50px">
+      <v-system-bar color="primary">
+        <v-icon class="ms-2" icon="mdi-wifi-strength-4"></v-icon>
+
+        <v-icon class="ms-2" icon="mdi-signal-cellular-outline"></v-icon>
+
+        <v-icon class="ms-2" icon="mdi-battery"></v-icon>
+
+        <span class="ms-2">08:30</span>
+      </v-system-bar>
+    </v-layout>
+
+    <v-layout style="height: 50px">
+      <v-system-bar color="red-lighten-2">
+        <v-icon class="ms-2" icon="mdi-wifi-strength-2"></v-icon>
+
+        <v-icon class="ms-2" icon="mdi-signal-cellular-outline"></v-icon>
+
+        <v-icon class="ms-2" icon="mdi-battery"></v-icon>
+
+        <span class="ms-2">18:30</span>
+      </v-system-bar>
+    </v-layout>
+
+    <v-layout style="height: 50px">
+      <v-system-bar color="indigo-darken-2">
+        <v-icon class="ms-2" icon="mdi-wifi-strength-3"></v-icon>
+
+        <v-icon class="ms-2" icon="mdi-signal-cellular-outline"></v-icon>
+
+        <v-icon class="ms-2" icon="mdi-battery"></v-icon>
+
+        <span class="ms-2">13:24</span>
+      </v-system-bar>
+    </v-layout>
+  </div>
+  <div div class="text-center">
+    <v-btn color="indigo">click me</v-btn>
+    <v-btn color="primary">Hello Vuetify from Vincent I am here </v-btn>
+  </div>
+  <div class="parent">
+    <h1>Test</h1>
+  </div>
+</template>
+<!-- <template>
   <div div class="text-center">
     <v-btn color="indigo">click me</v-btn>
     <v-btn color="primary">Hello Vuetify from Vincent I am here </v-btn>
@@ -69,7 +115,7 @@
       </v-fade-transition>
     </template>
   </v-list-item>
-</template>
+</template> -->
 
 <script setup>
 import { shallowRef } from "vue";
